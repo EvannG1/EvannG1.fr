940d7d40fdd4d8750dcd8b765808cf94f286b494
Evann Gehin <evann.gehin@proton.me>
2022-11-03 23:29:32 +0100

    Created models and migrations

---
+++ database/migrations/2022_11_03_222604_create_degrees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('degrees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('school_name');
+            $table->string('school_website')->nullable();
+            $table->string('degree_image');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('degrees');
+    }
+};
---
+++ database/migrations/2022_11_03_222611_create_experiences_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('city');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('company_logo');
+            $table->string('company_website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+};

1b2d684d23220f58d0c81f160d9de3be5aa6a601
Evann G <33735118+EvannG1@users.noreply.github.com>
2024-04-30 21:26:41 +0200

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,8 +4,8 @@
     <section id="about">
         <p class="title">About</p>
         <p class="content">
-            President of the <a href="https://alpasso.fr" target="_blank" rel="noreferrer noopener">Alpasso association</a>, self-taught and passionate about web application development and server administration for more than 5 years.<br>
-            I'm currently working as a Linux Systems Administrator at <a href="https://gamestream.biz" target="_blank" rel="noreferrer noopener">Gamestream</a>.<br>
+            Former President of the <a href="https://alpasso.fr" target="_blank" rel="noreferrer noopener">Alpasso association</a>, self-taught and passionate about web application development and server administration for more than 10 years.<br>
+            I'm currently working as a Linux System Administrator at <a href="https://miratlas.com" target="_blank" rel="noreferrer noopener">Miratlas</a>.<br>
         </p>
     </section>
 
@@ -26,21 +26,21 @@
         <div class="row">
             <div id="hardware" class="six columns">
                 <p>Primary computer hardware :</p>
-                <li>CPU : i5-9600KF</li>
-                <li>GPU : RTX 2070 Super</li>
-                <li>RAM : 32GB (4x8GB) DDR4 - 3000MHz</li>
-                <li>SSD NVMe 1TB + SSD 1TB + HDD 1TB</li>
-                <li>Motherboard : Gigabyte Z390UD</li>
-                <li>Power supply : Seasonic 650W</li>
-                <li>Case : Antec NX600</li>
+                <li>CPU : i7-13700KF @5.4GHz</li>
+                <li>GPU : RTX 4080</li>
+                <li>RAM : 32GB (2x16GB) DDR5 - 5200MHz</li>
+                <li>SSD NVMe 2TB + SSD NVMe 1TB</li>
+                <li>Motherboard : B760 GAMING PLUS WIFI</li>
+                <li>Power supply : M.RED ATX 1050W</li>
+                <li>Case : M.RED ELITE BLACK Rainbow ARGB</li>
             </div>
             <div id="stuffs" class="six columns">
                 <p>Other stuffs :</p>
-                <li>Screens : Gigabyte 27" G27QC-A (165Hz)</li>
-                <li>Workstation : MacBook Pro i9</li>
-                <li>Laptop : MacBook Air M2</li>
+                <li>Screens : Gigabyte 27" G27QC-A (2K @165Hz)</li>
+                <li>Workstation : MacBook Air M2 (16GB RAM & 512GB SSD)</li>
+                <li>Laptop : MacBook Air M2 (8GB RAM & 256GB SSD)</li>
                 <li>Home Server : Dell PowerEdge T320</li>
-                <li>Smartphone : iPhone 13 Pro</li>
+                <li>Smartphone : iPhone 15 Pro</li>
                 <li>Smartwatch : Apple Watch SE</li>
                 <li>Headset : HyperX Cloud II</li>
             </div>
